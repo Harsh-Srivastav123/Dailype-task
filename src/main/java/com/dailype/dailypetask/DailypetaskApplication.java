a11a62b74b0c160e34b411166413754adbb8e045
Harsh-Srivastav123 <114308429+Harsh-Srivastav123@users.noreply.github.com>
2024-06-05 01:16:51 +0530

    Initial Commit

---
+++ src/main/java/com/dailype/dailypetask/DailypetaskApplication.java
@@ -0,0 +1,13 @@
+package com.dailype.dailypetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailypetaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailypetaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailype/dailypetask/DailypetaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailype.dailypetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailypetaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
