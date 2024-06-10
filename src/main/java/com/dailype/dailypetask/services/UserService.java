2b05cdf2a40703cef087861eeb25ba9b980c4c01
Harsh Raj Srivastav <114308429+Harsh-Srivastav123@users.noreply.github.com>
2024-06-11 02:56:15 +0530

    Resolved updateUser glitch

--- src/main/java/com/dailype/dailypetask/services/UserService.java
+++ src/main/java/com/dailype/dailypetask/services/UserService.java
@@ -2,7 +2,6 @@ package com.dailype.dailypetask.services;
 
 import com.dailype.dailypetask.dao.ManagerDao;
 import com.dailype.dailypetask.dao.UserDao;
-import com.dailype.dailypetask.entity.Manager;
 import com.dailype.dailypetask.entity.User;
 import com.dailype.dailypetask.exceptions.BadRequestException;
 import com.dailype.dailypetask.exceptions.NotFoundException;
@@ -33,131 +32,138 @@ public class UserService {
 
     public String createUser(UserDto userDto) {
 
-        if(userDto.getMobNum().length()==10){
-            userDto.setMobNum("+91"+userDto.getMobNum());
+        if (userDto.getMobNum().length() == 10) {
+            userDto.setMobNum("+91" + userDto.getMobNum());
         }
 
-        if( userDto.getManagerId()!=null && !managerDao.existsById(userDto.getManagerId())){
+        if (userDto.getManagerId() != null && !managerDao.existsById(userDto.getManagerId())) {
             throw new NotFoundException("manager is not found in database check the id carefully !!");
         }
 
         log.info(String.valueOf(userDao.existsByMobNum(userDto.getMobNum())));
-        if(userDao.existsByMobNum(userDto.getMobNum())){
+        if (userDao.existsByMobNum(userDto.getMobNum())) {
             throw new BadRequestException("mobile no already exist !!");
         }
 
         userDto.setCreatedAt(new Date());
         userDto.setActive(true);
         userDto.setPanNum(userDto.getPanNum().toUpperCase());
-        User user=modelMapper.map(userDto,User.class);
-        String userId=userDao.save(user).getUserId();
-        log.info("user created successfully  "+ userId);
+        User user = modelMapper.map(userDto, User.class);
+        String userId = userDao.save(user).getUserId();
+        log.info("user created successfully  " + userId);
         return userId;
     }
 
     public Object getUserWithManagerId(String managerId) {
 
-        log.info("All user having manager_id  "+managerId);
-        return userDao.findByManagerId(managerId).stream().map(object->modelMapper.map(object,UserDto.class)).collect(Collectors.toList());
+        log.info("All user having manager_id  " + managerId);
+        return userDao.findByManagerId(managerId).stream().map(object -> modelMapper.map(object, UserDto.class)).collect(Collectors.toList());
     }
 
     public Object getUserByMobNum(String mobNum) {
         String mobile;
-        if(mobNum.length()==10){
-            mobile="+91"+mobNum;
+        if (mobNum.length() == 10) {
+            mobile = "+91" + mobNum;
+        } else {
+            mobile = mobNum;
         }
-        else {
-            mobile=mobNum;
+        if (!userDao.existsByMobNum(mobile)) {
+            throw new NotFoundException("user not found with mob_num");
         }
-        if(!userDao.existsByMobNum(mobile)){
-           throw new NotFoundException("user not found with mob_num");
-        }
-        log.info("user having mob_num  "+mobile);
-        return modelMapper.map(userDao.findByMobNum(mobile),UserDto.class);
+        log.info("user having mob_num  " + mobile);
+        return modelMapper.map(userDao.findByMobNum(mobile), UserDto.class);
     }
 
     public Object getUserById(String userId) {
 
-        if(!userDao.existsById(userId)){
+        if (!userDao.existsById(userId)) {
             throw new BadRequestException("user not found with user_id");
         }
-        log.info("user having user_id  "+userId);
-        return modelMapper.map(userDao.findById(userId),UserDto.class);
+        log.info("user having user_id  " + userId);
+        return modelMapper.map(userDao.findById(userId), UserDto.class);
     }
 
     public String deleteUser(String userId) {
-        if(!userDao.existsById(userId)){
+        if (!userDao.existsById(userId)) {
             throw new NotFoundException("user not found check the user_id carefully");
         }
         userDao.deleteById(userId);
-        log.info("user deleted successfully   "+userId);
+        log.info("user deleted successfully   " + userId);
         return userId;
     }
 
     public String deleteByMobNum(String mobNum) {
 
         String mobile;
-        if(mobNum.length()==10){
-            mobile="+91"+mobNum;
+        if (mobNum.length() == 10) {
+            mobile = "+91" + mobNum;
         }
-        mobile=mobNum;
-        if(!userDao.existsByMobNum(mobile)){
+        mobile = mobNum;
+        if (!userDao.existsByMobNum(mobile)) {
             throw new NotFoundException("user not found check the mob_num carefully");
         }
-        User user=userDao.findByMobNum(mobile);
+        User user = userDao.findByMobNum(mobile);
         userDao.delete(user);
-        log.info("user deleted successfully   "+user.getUserId());
+        log.info("user deleted successfully   " + user.getUserId());
         return user.getUserId();
     }
 
     public String updateUser(@Valid UserDto userDto) {
-        if(!userDao.existsById(userDto.getUserId())){
+        log.info("updating user user_id = " + userDto.getUserId());
+        if (!userDao.existsById(userDto.getUserId())) {
             throw new NotFoundException("user not found check the user_id carefully");
         }
-        if(userDto.getManagerId()!=null && !managerDao.existsById(userDto.getManagerId())){
+        if (userDto.getManagerId() != null && !managerDao.existsById(userDto.getManagerId())) {
             throw new NotFoundException("manager not found check the manager_id carefully");
         }
-        User user=userDao.findById(userDto.getUserId()).get();
-        if(userDto.getManagerId()==null ||user.getManagerId().equals(userDto.getManagerId())){
-            return userDao.save(userUpdate(userDto,user)).getUserId();
+        User user = userDao.findById(userDto.getUserId()).get();
+        if (userDto.getManagerId() == null) {
+            return userDao.save(userUpdate(userDto, user)).getUserId();
         }
-        else {
-            if(user.getUpdatedAt()==null){
-                user.setUpdatedAt(new Date());
-                return userDao.save(userUpdate(userDto,user)).getUserId();
+        if (user.getManagerId() != null ) {
+            if(user.getManagerId().equals(userDto.getManagerId())){
+                return userDao.save(userUpdate(userDto, user)).getUserId();
             }
             else {
-                log.info("creating new user because manager_id is updated once !!");
-               user.setActive(false);
-               User user1=new User();
-               user1.setPanNum(user.getPanNum());
-               user1.setMobNum(user.getMobNum());
-               user1.setFullName(user.getFullName());
-               user1.setCreatedAt(user.getCreatedAt());
-               user1.setUpdatedAt(new Date());
-               user1.setActive(true);
-               return userDao.save(userUpdate(userDto,user1)).getUserId();
+                if (user.getUpdatedAt() == null) {
+                    user.setUpdatedAt(new Date());
+                    return userDao.save(userUpdate(userDto, user)).getUserId();
+                } else {
+                    log.info("creating new user because manager_id is updated once !!");
+                    user.setActive(false);
+                    User user1 = new User();
+                    user1.setPanNum(user.getPanNum());
+                    user1.setMobNum(user.getMobNum());
+                    user1.setFullName(user.getFullName());
+                    user1.setCreatedAt(user.getCreatedAt());
+                    user1.setUpdatedAt(new Date());
+                    user1.setActive(true);
+                    return userDao.save(userUpdate(userDto, user1)).getUserId();
+                }
             }
+        }
+        else {
+            user.setUpdatedAt(new Date());
+            return userDao.save(userUpdate(userDto, user)).getUserId();
         }
     }
 
     private User userUpdate(UserDto userDto, User user) {
 
-        if(userDto.getFullName()!=null){
+        if (userDto.getFullName() != null) {
             user.setFullName(userDto.getFullName());
         }
-        if(userDto.getPanNum()!=null){
+        if (userDto.getPanNum() != null) {
             user.setPanNum(userDto.getPanNum().toUpperCase());
         }
-        if(userDto.getMobNum()!=null){
-            if(userDto.getMobNum().length()==10){
-                user.setMobNum("+91"+userDto.getMobNum());
-            }
-            else {
+        if (userDto.getMobNum() != null) {
+            if (userDto.getMobNum().length() == 10) {
+                user.setMobNum("+91" + userDto.getMobNum());
+            } else {
                 user.setMobNum(userDto.getMobNum());
             }
         }
-        if(userDto.getManagerId()!=null){
+        if (userDto.getManagerId() != null) {
             user.setManagerId(userDto.getManagerId());
         }
 
@@ -165,14 +171,14 @@ public class UserService {
     }
 
     public void userUpdateUsingList(List<UserDto> userList) {
-        for (UserDto userDto:userList){
-            if(!userDao.existsById(userDto.getUserId())){
+        for (UserDto userDto : userList) {
+            if (!userDao.existsById(userDto.getUserId())) {
                 throw new NotFoundException("user not found check the user_id carefully");
             }
-            User user=userDao.findById(userDto.getUserId()).get();
-            if(!user.getPanNum().equals(userDto.getPanNum())|| !user.getFullName().equals(userDto.getFullName()) || !user.getMobNum().equals(userDto.getMobNum())){
+            User user = userDao.findById(userDto.getUserId()).get();
+            if (!user.getPanNum().equals(userDto.getPanNum()) || !user.getFullName().equals(userDto.getFullName()) || !user.getMobNum().equals(userDto.getMobNum())) {
                 throw new BadRequestException("only manager key is updated in bulk list");
-            }else {
+            } else {
                 user.setManagerId(userDto.getManagerId());
             }
             user.setUpdatedAt(new Date());
@@ -181,7 +187,7 @@ public class UserService {
     }
 
     public Object findUserforTest(String userId) {
-        if(!userDao.existsById(userId)){
+        if (!userDao.existsById(userId)) {
             throw new NotFoundException("user not found check the id carefully!!");
         }
         return userDao.findById(userId);
