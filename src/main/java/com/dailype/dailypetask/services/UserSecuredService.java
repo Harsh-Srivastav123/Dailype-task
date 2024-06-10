9fab3db6111cca05ddad36b27e0c48014e05e341
Harsh Raj Srivastav <114308429+Harsh-Srivastav123@users.noreply.github.com>
2024-06-11 03:11:46 +0530

    Update UserSecuredService.java resolve update glitch

--- src/main/java/com/dailype/dailypetask/services/UserSecuredService.java
+++ src/main/java/com/dailype/dailypetask/services/UserSecuredService.java
@@ -3,7 +3,6 @@ package com.dailype.dailypetask.services;
 import com.dailype.dailypetask.dao.ManagerDao;
 import com.dailype.dailypetask.dao.RefreshTokenDao;
 import com.dailype.dailypetask.dao.UserSecuredDao;
-
 import com.dailype.dailypetask.dao.VerifyTokenDao;
 import com.dailype.dailypetask.entity.RefreshToken;
 import com.dailype.dailypetask.entity.UserSecured;
@@ -11,7 +10,6 @@ import com.dailype.dailypetask.entity.VerifyToken;
 import com.dailype.dailypetask.exceptions.BadRequestException;
 import com.dailype.dailypetask.exceptions.NotFoundException;
 import com.dailype.dailypetask.model.UserSecuredDto;
-
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -28,164 +26,163 @@ import java.util.stream.Collectors;
 @Slf4j
 public class UserSecuredService {
 
+    public static final int REFRESH_TOKEN_EXPIRATION_TIME = 3600;//expiration task
     @Autowired
     UserSecuredDao userSecuredDao;
-
     @Autowired
     ManagerDao managerDao;
-
     @Autowired
     ModelMapper modelMapper;
-
     @Autowired
     PasswordEncoder passwordEncoder;
-
     @Autowired
     VerifyTokenDao verifyTokenDao;
-
     @Autowired
     AwsService awsServices;
-
     @Autowired
     RefreshTokenDao refreshTokenDao;
-    public static final int REFRESH_TOKEN_EXPIRATION_TIME=3600;//expiration task
 
-    public String createUser(@Valid  UserSecuredDto userSecuredDto, MultipartFile file) {
-        if(Objects.equals(file.getContentType(), "image/png") || Objects.equals(file.getContentType(), "image/jpeg")){
-            String fileName=UUID.randomUUID().toString()+"_"+file.getName();
-            awsServices.uploadMedia(file,fileName);
+    public String createUser(@Valid UserSecuredDto userSecuredDto, MultipartFile file) {
+        if (Objects.equals(file.getContentType(), "image/png") || Objects.equals(file.getContentType(), "image/jpeg")) {
+            String fileName = UUID.randomUUID() + "_" + file.getName();
+            awsServices.uploadMedia(file, fileName);
             userSecuredDto.setImageUrl(fileName);
         }
-        if(userSecuredDao.findByUserName(userSecuredDto.getUserName()).isPresent()){
+        if (userSecuredDao.findByUserName(userSecuredDto.getUserName()).isPresent()) {
             throw new BadRequestException("user already exists with the same user_name !!");
         }
 
-        if(userSecuredDto.getMobNum().length()==10){
-            userSecuredDto.setMobNum("+91"+userSecuredDto.getMobNum());
+        if (userSecuredDto.getMobNum().length() == 10) {
+            userSecuredDto.setMobNum("+91" + userSecuredDto.getMobNum());
         }
 
-        if( userSecuredDto.getManagerId()!=null && !managerDao.existsById(userSecuredDto.getManagerId())){
+        if (userSecuredDto.getManagerId() != null && !managerDao.existsById(userSecuredDto.getManagerId())) {
             throw new NotFoundException("manager is not found in database check the id carefully !!");
         }
 
         log.info(String.valueOf(userSecuredDao.existsByMobNum(userSecuredDto.getMobNum())));
-        if(userSecuredDao.existsByMobNum(userSecuredDto.getMobNum())){
+        if (userSecuredDao.existsByMobNum(userSecuredDto.getMobNum())) {
             throw new BadRequestException("mobile no already exist !!");
         }
         userSecuredDto.setPassword(passwordEncoder.encode(userSecuredDto.getPassword()));
         userSecuredDto.setCreatedAt(new Date());
         userSecuredDto.setActive(true);
         userSecuredDto.setPanNum(userSecuredDto.getPanNum().toUpperCase());
-        UserSecured UserSecured=modelMapper.map(userSecuredDto,UserSecured.class);
-        String userSecuredId=userSecuredDao.save(UserSecured).getUserSecuredId();
-        log.info("UserSecured created successfully  "+ userSecuredId);
+        UserSecured UserSecured = modelMapper.map(userSecuredDto, UserSecured.class);
+        String userSecuredId = userSecuredDao.save(UserSecured).getUserSecuredId();
+        log.info("UserSecured created successfully  " + userSecuredId);
         return userSecuredId;
     }
 
 
     public Object getUserWithManagerId(String managerId) {
 
-        log.info("All UserSecured having manager_id  "+managerId);
-        return userSecuredDao.findByManagerId(managerId).stream().map(object->modelMapper.map(object,UserSecuredDto.class)).collect(Collectors.toList());
+        log.info("All UserSecured having manager_id  " + managerId);
+        return userSecuredDao.findByManagerId(managerId).stream().map(object -> modelMapper.map(object, UserSecuredDto.class)).collect(Collectors.toList());
     }
 
     public Object getUserByMobNum(String mobNum) {
         String mobile;
-        if(mobNum.length()==10){
-            mobile="+91"+mobNum;
+        if (mobNum.length() == 10) {
+            mobile = "+91" + mobNum;
+        } else {
+            mobile = mobNum;
         }
-        else {
-            mobile=mobNum;
-        }
-        if(!userSecuredDao.existsByMobNum(mobile)){
+        if (!userSecuredDao.existsByMobNum(mobile)) {
             throw new NotFoundException("UserSecured not found with mob_num");
         }
-        log.info("UserSecured having mob_num  "+mobile);
-        return modelMapper.map(userSecuredDao.findByMobNum(mobile),UserSecuredDto.class);
+        log.info("UserSecured having mob_num  " + mobile);
+        return modelMapper.map(userSecuredDao.findByMobNum(mobile), UserSecuredDto.class);
     }
 
     public Object getUserById(String userSecuredId) {
 
-        if(!userSecuredDao.existsById(userSecuredId)){
+        if (!userSecuredDao.existsById(userSecuredId)) {
             throw new BadRequestException("UserSecured not found with UserSecured_id");
         }
-        log.info("UserSecured having UserSecured_id  "+userSecuredId);
-        return modelMapper.map(userSecuredDao.findById(userSecuredId),UserSecuredDto.class);
+        log.info("UserSecured having UserSecured_id  " + userSecuredId);
+        return modelMapper.map(userSecuredDao.findById(userSecuredId), UserSecuredDto.class);
     }
 
     public String deleteUser(String userSecuredId) {
-        if(!userSecuredDao.existsById(userSecuredId)){
+        if (!userSecuredDao.existsById(userSecuredId)) {
             throw new NotFoundException("UserSecured not found check the UserSecured_id carefully");
         }
         userSecuredDao.deleteById(userSecuredId);
-        log.info("UserSecured deleted successfully   "+userSecuredId);
+        log.info("UserSecured deleted successfully   " + userSecuredId);
         return userSecuredId;
     }
 
     public String deleteByMobNum(String mobNum) {
 
         String mobile;
-        if(mobNum.length()==10){
-            mobile="+91"+mobNum;
+        if (mobNum.length() == 10) {
+            mobile = "+91" + mobNum;
         }
-        mobile=mobNum;
-        if(!userSecuredDao.existsByMobNum(mobile)){
+        mobile = mobNum;
+        if (!userSecuredDao.existsByMobNum(mobile)) {
             throw new NotFoundException("UserSecured not found check the mob_num carefully");
         }
-        UserSecured UserSecured=userSecuredDao.findByMobNum(mobile);
+        UserSecured UserSecured = userSecuredDao.findByMobNum(mobile);
         userSecuredDao.delete(UserSecured);
-        log.info("UserSecured deleted successfully   "+UserSecured.getUserSecuredId());
+        log.info("UserSecured deleted successfully   " + UserSecured.getUserSecuredId());
         return UserSecured.getUserSecuredId();
     }
 
     public String updateUser(@Valid UserSecuredDto userSecuredDto) {
-        if(!userSecuredDao.existsById(userSecuredDto.getUserSecuredId())){
+        if (!userSecuredDao.existsById(userSecuredDto.getUserSecuredId())) {
             throw new NotFoundException("UserSecured not found check the User_secured_id carefully");
         }
-        if(userSecuredDto.getManagerId()!=null && !managerDao.existsById(userSecuredDto.getManagerId())){
+        if (userSecuredDto.getManagerId() != null && !managerDao.existsById(userSecuredDto.getManagerId())) {
             throw new NotFoundException("manager not found check the manager_id carefully");
         }
-        UserSecured UserSecured=userSecuredDao.findById(userSecuredDto.getUserSecuredId()).get();
-        if(userSecuredDto.getManagerId()==null ||UserSecured.getManagerId().equals(userSecuredDto.getManagerId())){
-            return userSecuredDao.save(UserSecuredUpdate(userSecuredDto,UserSecured)).getUserSecuredId();
+        UserSecured userSecured = userSecuredDao.findById(userSecuredDto.getUserSecuredId()).get();
+        if (userSecuredDto.getManagerId() == null) {
+            return userSecuredDao.save(UserSecuredUpdate(userSecuredDto, userSecured)).getUserSecuredId();
         }
-        else {
-            if(UserSecured.getUpdatedAt()==null){
-                UserSecured.setUpdatedAt(new Date());
-                return userSecuredDao.save(UserSecuredUpdate(userSecuredDto,UserSecured)).getUserSecuredId();
+
+        if (userSecured.getManagerId() != null) {
+            if (userSecured.getManagerId().equals(userSecuredDto.getManagerId())) {
+                return userSecuredDao.save(UserSecuredUpdate(userSecuredDto, userSecured)).getUserSecuredId();
+            } else {
+                if (userSecured.getUpdatedAt() == null) {
+                    userSecured.setUpdatedAt(new Date());
+                    return userSecuredDao.save(UserSecuredUpdate(userSecuredDto, userSecured)).getUserSecuredId();
+                } else {
+                    log.info("creating new user_secured because manager_id is updated once !!");
+                    userSecured.setActive(false);
+                    UserSecured userSecured1 = new UserSecured();
+                    userSecured1.setPanNum(userSecured.getPanNum());
+                    userSecured1.setMobNum(userSecured.getMobNum());
+                    userSecured1.setFullName(userSecured.getFullName());
+                    userSecured1.setCreatedAt(userSecured.getCreatedAt());
+                    userSecured1.setUpdatedAt(new Date());
+                    userSecured1.setActive(true);
+                    return userSecuredDao.save(UserSecuredUpdate(userSecuredDto, userSecured1)).getUserSecuredId();
+                }
             }
-            else {
-                log.info("creating new user_secured because manager_id is updated once !!");
-                UserSecured.setActive(false);
-                UserSecured UserSecured1=new UserSecured();
-                UserSecured1.setPanNum(UserSecured.getPanNum());
-                UserSecured1.setMobNum(UserSecured.getMobNum());
-                UserSecured1.setFullName(UserSecured.getFullName());
-                UserSecured1.setCreatedAt(UserSecured.getCreatedAt());
-                UserSecured1.setUpdatedAt(new Date());
-                UserSecured1.setActive(true);
-                return userSecuredDao.save(UserSecuredUpdate(userSecuredDto,UserSecured1)).getUserSecuredId();
-            }
+        } else {
+            userSecured.setUpdatedAt(new Date());
+            return userSecuredDao.save(UserSecuredUpdate(userSecuredDto, userSecured)).getUserSecuredId();
         }
     }
 
     private UserSecured UserSecuredUpdate(UserSecuredDto userSecuredDto, UserSecured UserSecured) {
 
-        if(userSecuredDto.getFullName()!=null){
+        if (userSecuredDto.getFullName() != null) {
             UserSecured.setFullName(userSecuredDto.getFullName());
         }
-        if(userSecuredDto.getPanNum()!=null){
+        if (userSecuredDto.getPanNum() != null) {
             UserSecured.setPanNum(userSecuredDto.getPanNum().toUpperCase());
         }
-        if(userSecuredDto.getMobNum()!=null){
-            if(userSecuredDto.getMobNum().length()==10){
-                UserSecured.setMobNum("+91"+userSecuredDto.getMobNum());
-            }
-            else {
+        if (userSecuredDto.getMobNum() != null) {
+            if (userSecuredDto.getMobNum().length() == 10) {
+                UserSecured.setMobNum("+91" + userSecuredDto.getMobNum());
+            } else {
                 UserSecured.setMobNum(userSecuredDto.getMobNum());
             }
         }
-        if(userSecuredDto.getManagerId()!=null){
+        if (userSecuredDto.getManagerId() != null) {
             UserSecured.setManagerId(userSecuredDto.getManagerId());
         }
 
@@ -193,14 +190,14 @@ public class UserSecuredService {
     }
 
     public void UserUpdateUsingList(List<UserSecuredDto> UserSecuredList) {
-        for (UserSecuredDto userSecuredDto:UserSecuredList){
-            if(!userSecuredDao.existsById(userSecuredDto.getUserSecuredId())){
+        for (UserSecuredDto userSecuredDto : UserSecuredList) {
+            if (!userSecuredDao.existsById(userSecuredDto.getUserSecuredId())) {
                 throw new NotFoundException("UserSecured not found check the user_secured_id carefully");
             }
-            UserSecured UserSecured=userSecuredDao.findById(userSecuredDto.getUserSecuredId()).get();
-            if(!UserSecured.getPanNum().equals(userSecuredDto.getPanNum())|| !UserSecured.getFullName().equals(userSecuredDto.getFullName()) || !UserSecured.getMobNum().equals(userSecuredDto.getMobNum())){
+            UserSecured UserSecured = userSecuredDao.findById(userSecuredDto.getUserSecuredId()).get();
+            if (!UserSecured.getPanNum().equals(userSecuredDto.getPanNum()) || !UserSecured.getFullName().equals(userSecuredDto.getFullName()) || !UserSecured.getMobNum().equals(userSecuredDto.getMobNum())) {
                 throw new BadRequestException("only manager key is updated in bulk list");
-            }else {
+            } else {
                 UserSecured.setManagerId(userSecuredDto.getManagerId());
             }
             UserSecured.setUpdatedAt(new Date());
@@ -209,16 +206,16 @@ public class UserSecuredService {
     }
 
     public Object getUserIdByUsername(String userName) {
-        Optional<UserSecured> userSecured=userSecuredDao.findByUserName(userName);
-        if(userSecured.isEmpty()){
+        Optional<UserSecured> userSecured = userSecuredDao.findByUserName(userName);
+        if (userSecured.isEmpty()) {
             throw new BadRequestException("unable to find user_secured by username !!");
         }
         return userSecured.get().getUserSecuredId();
     }
 
     public void saveVerificationToken(UserSecuredDto userSecuredDto, String token) {
-        UserSecured userSecured=userSecuredDao.findByUserName(userSecuredDto.getUserName()).get();
-        verifyTokenDao.save(new VerifyToken(token,userSecured));
+        UserSecured userSecured = userSecuredDao.findByUserName(userSecuredDto.getUserName()).get();
+        verifyTokenDao.save(new VerifyToken(token, userSecured));
     }
 
     public int validateToken(String token) {
@@ -241,44 +238,40 @@ public class UserSecuredService {
         return 3;
     }
 
-    public byte[] getImage(String userSecuredId){
-        if(!userSecuredDao.existsById(userSecuredId)){
+    public byte[] getImage(String userSecuredId) {
+        if (!userSecuredDao.existsById(userSecuredId)) {
             throw new BadRequestException("User_secured doesn't exists");
         }
-        String fileName=userSecuredDao.findById(userSecuredId).get().getImageUrl();
+        String fileName = userSecuredDao.findById(userSecuredId).get().getImageUrl();
         return awsServices.downloadFile(fileName);
     }
 
     public String createRefreshToken(String username) {
-        RefreshToken refreshTokenTemp=refreshTokenDao.findByUserSecured(userSecuredDao.findByUserName(username).get());
-        if(refreshTokenTemp!=null){
+        RefreshToken refreshTokenTemp = refreshTokenDao.findByUserSecured(userSecuredDao.findByUserName(username).get());
+        if (refreshTokenTemp != null) {
             refreshTokenTemp.setUserSecured(null);
             refreshTokenDao.delete(refreshTokenDao.save(refreshTokenTemp));
         }
-        RefreshToken refreshToken = RefreshToken.builder()
-                .userSecured(userSecuredDao.findByUserName(username).get())
-                .token(UUID.randomUUID().toString())
-                .expiryDate(Instant.now().plusMillis(60000*REFRESH_TOKEN_EXPIRATION_TIME))
-                .build();
+        RefreshToken refreshToken = RefreshToken.builder().userSecured(userSecuredDao.findByUserName(username).get()).token(UUID.randomUUID().toString()).expiryDate(Instant.now().plusMillis(60000 * REFRESH_TOKEN_EXPIRATION_TIME)).build();
         return refreshTokenDao.save(refreshToken).getToken();
     }
 
     public UserSecuredDto verifyExpiration(String refreshToken) {
-        if(!refreshTokenDao.existsByToken(refreshToken)){
+        if (!refreshTokenDao.existsByToken(refreshToken)) {
             throw new BadRequestException("Unable to associate user with this token !!");
         }
-        RefreshToken token=refreshTokenDao.findByToken(refreshToken);
+        RefreshToken token = refreshTokenDao.findByToken(refreshToken);
 
         if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
             refreshTokenDao.delete(token);
             throw new BadRequestException(token.getToken() + " Refresh token was expired. Please make a new signin request");
         }
-        return modelMapper.map(token.getUserSecured(),UserSecuredDto.class);
+        return modelMapper.map(token.getUserSecured(), UserSecuredDto.class);
     }
 
 
     public Object getRefreshToken(String userName) {
-        if(userSecuredDao.findByUserName(userName).isEmpty()){
+        if (userSecuredDao.findByUserName(userName).isEmpty()) {
             throw new BadRequestException("user_secured doesn't exists !!");
         }
         return refreshTokenDao.findByUserSecured(userSecuredDao.findByUserName(userName).get()).getToken();
